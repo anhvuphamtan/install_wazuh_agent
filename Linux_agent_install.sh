672f4a8d04a2a8db27ea4e4093d2d0c63ec26650
Anh Vũ <33659373+anhvuphamtan@users.noreply.github.com>
2023-12-12 15:44:55 +0700

    Update Linux_agent_install.sh

--- Linux_agent_install.sh
+++ Linux_agent_install.sh
@@ -6,8 +6,6 @@ manager="wazuh-uat-agent.styl.solutions"
 
 curl -so wazuh-agent.deb https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_4.5.4-1_amd64.deb && sudo WAZUH_MANAGER=$manager WAZUH_AGENT_GROUP=$agent_group WAZUH_AGENT_NAME=$agent_name dpkg -i ./wazuh-agent.deb
 
-sudo sed -i '/<client>/,/<\/client>/ s|<server>|<server>\n    <retry_interval>600</retry_interval>|' /var/ossec/etc/ossec.conf
-
 echo "Install Wazuh agent successfully"
 sleep 5
 
