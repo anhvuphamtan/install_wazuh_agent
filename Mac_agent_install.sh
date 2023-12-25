b9e788d1d0e62e891f729abee244d015e36d4fd3
Anh Vũ <33659373+anhvuphamtan@users.noreply.github.com>
2023-12-25 14:09:41 +0700

    Update Mac_agent_install.sh

--- Mac_agent_install.sh
+++ Mac_agent_install.sh
@@ -8,7 +8,7 @@ read AGENT_GROUP
 curl -O https://packages.wazuh.com/4.x/macos/wazuh-agent-4.7.0-1.arm64.pkg
 
 echo "WAZUH_MANAGER='$MANAGER' && WAZUH_REGISTRATION_SERVER='$MANAGER' && WAZUH_AGENT_NAME='$AGENT_NAME' && \
-WAZUH_AGENT_GROUP='$AGENT_GROUP'" > /tmp/wazuh_envs && installer -pkg ./wazuh-agent-4.5.4-1.arm64.pkg -target /
+WAZUH_AGENT_GROUP='$AGENT_GROUP'" > /tmp/wazuh_envs && installer -pkg ./wazuh-agent-4.7.0-1.arm64.pkg -target /
 
 sleep 2
 
